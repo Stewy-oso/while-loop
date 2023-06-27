e46c4d2a13f40dab67a0d0bafb4efb317618854c
Stewy-oso <haplesstew5034@gmail.com>
2023-06-27 17:15:52 +0100

    first commit

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿namespace for_loop;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        ///Appearance.
+        Console.Title = "Other Loop";
+        Console.ForegroundColor = ConsoleColor.Red;
+        
+        Random numberGen = new Random();
+
+        int roll = 0;
+        int attempts = 0;
+
+        Console.WriteLine("Press any key to roll the dice.");
+
+        while(roll != 6)
+        {
+            Console.ReadKey();
+            roll = numberGen.Next(1, 7);
+            Console.WriteLine("You rolled: " + roll);
+            attempts++;
+        }
+
+        Console.WriteLine("It took you " + attempts + " attempts to roll a six.");
+
+        Console.ReadKey();
+    }
+}
